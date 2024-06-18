7b55ccf3586494e884bf67206b42f16c41d2289b
Heimer Martinez <hmartinez@cafetosoftware.com>
2024-06-17 21:02:44 -0500

    Start the Gutenberg Library plugin

---
+++ lab-gutenberg-blocks.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Lab Gutenberg Blocks
+Plugin URI: http://www.ejemplo.com/mi-primer-plugin
+Description: Lab of multiple plugins
+Version: 1.0.0
+Author: Heimer Martinez
+Author URI: https://www.linkedin.com/in/wordpress-dev-tech-seo-heimer-martinez/
+License: GPL2
+*/
+
+/*
+Copyright YEAR Tu Nombre (email@ejemplo.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+if (!defined('ABSPATH')) exit;
